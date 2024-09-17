51dbf8e53ea5a318d11b28c89394c9b431365df3
Tess Gauthier <tgauth@bu.edu>
2024-09-17 11:25:15 -0400

    update openssh banner

--- version.h
+++ version.h
@@ -1,6 +1,6 @@
 /* $OpenBSD: version.h,v 1.101 2024/03/11 04:59:47 djm Exp $ */
 
-#define SSH_VERSION	"OpenSSH_for_Windows_9.7"
+#define SSH_VERSION	"OpenSSH_for_Windows_9.8 Win32-OpenSSH-GitHub"
 
 #define SSH_PORTABLE	"p1"
 #define SSH_RELEASE	SSH_VERSION SSH_PORTABLE
